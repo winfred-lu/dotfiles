b3c9b571dfee3489d476b0fc53f4ce82c334c4ee
Winfred Lu <winfred.lu@gmail.com>
2025-08-28 09:30:15 +0800

    Remove tree-sitter grammar setting.
    TS grammer is now installed and compiled by orgmode.
    There is no need to setup TS grammer.
    Ref: https://github.com/nvim-orgmode/orgmode/issues/217#issuecomment-2026956347

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -1,5 +1,6 @@
 local M = {
   'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
   dependencies = {
     { 'nvim-treesitter/nvim-treesitter', lazy = true },
     { 'akinsho/org-bullets.nvim', lazy = true },
@@ -7,18 +8,6 @@ local M = {
   ft = { 'org', 'orgagenda' },
 
   config = function()
-    -- Load treesitter grammar for org
-    require('orgmode').setup_ts_grammar()
-
-    -- Setup treesitter
-    require('nvim-treesitter.configs').setup({
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'org' },
-      },
-      ensure_installed = { 'org' },
-    })
-
     -- Setup orgmode
     require('orgmode').setup({
       org_agenda_files = '~/org/**/*',
